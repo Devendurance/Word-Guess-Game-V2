3ec7e7b76d1dc94a8e39b1fefde32d06f280e478
Devendurance <enduranceudoh888@gmail.com>
2023-07-15 16:04:08 +0100

    updated word database

--- script.js
+++ script.js
@@ -1,6 +1,94 @@
 //Word and Hint
 const wordList = [
     {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "victory",
+        hint: "antonym of defeat"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "game played by two players"
+    },
+    {
+        word: "act",
+        hint: "the doing of a thing"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "silver",
+        hint: "a shiny and costly natural resource"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "matrix",
+        hint: "science fiction movie"
+    },
+    {
+        word: "bugs",
+        hint: "related to programming"
+    },
+    {
+        word: "poseidon",
+        hint: "greek god of water"
+    },
+    {
+        word: "ares",
+        hint: "greek god of war"
+    },
+    {
+        word: "tinubu",
+        hint: "nigerian king of the streets"
+    },
+    {
+        word: "venice",
+        hint: "italian city surrounded by water"
+    },
+    {
+        word: "khalifa",
+        hint: "a famous skyscrapper in dubai, also a title of leadership"
+    },
+    {
+        word: "chocolate",
+        hint: "sweet and brown"
+    },
+    {
         word: "mystery",
         hint: "something that is unknown or difficult to understand"
     },
@@ -101,7 +189,7 @@ const wordList = [
         hint: "🚕"
     },
     {
-        word: "traffic light",
+        word: "trafficlight",
         hint: "🚦"
     },
     {
